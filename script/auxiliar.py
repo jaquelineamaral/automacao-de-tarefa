6b078b4b2c36771da07350af7360eeb292bc48b9
Jaqueline <jaqueline1@ice.ufjf.br>
2024-04-22 22:27:40 -0300

    preenche campos automaticamente
    le cada produto do arquivo csv e preenche automaticamente o formulario

--- script/auxiliar.py
+++ script/auxiliar.py
@@ -1,7 +1,16 @@
 import pyautogui
 import time
+import pandas
+import os
 
 time.sleep(5)
 print(pyautogui.position())
 
+diretorio_atual = os.path.dirname(__file__)
+
+tabela = pandas.read_csv(os.path.join(diretorio_atual, '../arquivos/produtos.csv'))
+
+item = str(tabela.loc[1, 'obs'])
+
+print(len(item))
 
--- script/codigo.py
+++ script/codigo.py
@@ -5,7 +5,7 @@ import pandas
 import os
 
 # pausa a cada comando do pyautogui
-pyautogui.PAUSE = 3 
+pyautogui.PAUSE = 1 
 
 link = 'https://dlp.hashtagtreinamentos.com/python/intensivao/login'
 
@@ -30,21 +30,30 @@ pyautogui.press('enter')
 # pausa maoir em comando específico - 3 segundos
 time.sleep(3)
 
-pyautogui.click(x=509, y=376)
+pyautogui.click(x=509, y=374)
 pyautogui.write('sjaqueline987@gmail.com')
 
-# pyautogui.click(x=491, y=478)
 pyautogui.press('tab')
 pyautogui.write('admin123')
 
-pyautogui.click(x=654, y=545)
-# pyautogui.press('tab')
-# pyautogui.press('enter')
+pyautogui.press('tab')
+pyautogui.press('enter')
 time.sleep(3)
 
 # importar dados
 diretorio_atual = os.path.dirname(__file__)
 
 tabela = pandas.read_csv(os.path.join(diretorio_atual, '../arquivos/produtos.csv'))
-print(tabela)
 
+for linha in tabela.index:
+    pyautogui.click(x=552, y=237)
+
+    for coluna in tabela.columns:
+        item = tabela.loc[linha, coluna]
+
+        if not (coluna == 'obs' and pandas.isna(item)):
+            pyautogui.write(str(item))
+        pyautogui.press('tab')       
+    
+    pyautogui.press('enter')
+    pyautogui.scroll(5000)
